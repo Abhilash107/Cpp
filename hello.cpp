01803ebebc0bd8f0adda3fd83f00c404cd6a118d
Abhilash107 <abhilash00003108884@gmail.com>
2025-01-23 19:42:53 +0530

    refactor hello.cpp to improve namespace usage and add getNum function

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,29 @@
 #include <iostream>//preprocessor directors
 
-using namespace std;
+//using namespace std;//best practice to remove
 // namespace myCode{
 //     void display(){
 
 //     }
 // }
+using std::cout;//Using Declarations
+using std::endl;
+int getNum();// function declaration
 
 int main(){
     //myCode::display();
-    cout << "Hello World!";
+    //std::cout << "Hello World!" << std::endl;
+    cout << "Hello World!" << endl;
+    cout << getNum();
+
+    // << 
+    return 0;
+
+
+    
+
+}
+
+int getNum(){
+    return 1 + 1;
 }
