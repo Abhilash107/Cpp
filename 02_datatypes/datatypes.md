61038daaacee942d7912594b889e427203175ce9
Abhilash107 <abhilash00003108884@gmail.com>
2025-01-24 11:58:36 +0530

    add datatypes documentation and example C++ program

---
+++ 02_datatypes/datatypes.md
@@ -0,0 +1,8 @@
+# DataTypes:
+### 1. Primitive
+- int, float(6-7), double(15-16), char, boolean, void
+### 2. Derived
+- Array, function, pointer
+### 3. user defined
+- Structure, Union, Enum, Class, Typedef
+
---
+++ Intro/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+
+int main(){
+    int a = 10;
+    float b = 12.5;//6 - 7 places
+    double c = 12.5;// 15 - 16 places
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+    cout<<c<<endl;
+
+}
