a5a00cd6c0dd015a23697803f17c016805cb2fa6
Abhilash107 <abhilash00003108884@gmail.com>
2025-01-24 11:41:42 +0530

    add main.cpp with basic C++ structure and comments on identifiers and constants

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //cout << "hello" << endl;
+    int score = 10;
+    //identifier:
+    //In C++, an identifier is a name used to identify a variable, function, class, module, or any other user-defined item. Identifiers are fundamental to programming in C++ as they allow you to refer to various elements in your code.
+
+
+    const int userId = 124;//fixed value, can't be changed
+    //userId = 123;//expression must be a modifiable lvalue
+
+
+
+
+    return 0;
+    //data -> store and process -> display
+
+
+}
