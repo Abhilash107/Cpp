8cf5e0c67667df29f939cdd76c04918f2b32a3ad
Abhilash107 <abhilash00003108884@gmail.com>
2025-01-26 18:02:54 +0530

    add myFunctionTwo.cpp to demonstrate call by value in function parameters

---
+++ 06_functions/myFunctionTwo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void getNum(int num){
+    num = num + 5;
+    cout << "Number: " << num << endl;
+}
+
+
+
+int main(){
+    // call by value
+    int num = 10;
+    getNum(num);//15
+    cout << num << endl;//10
+
+
+
+
+
+    return 0;
+}
