6e71f226b9d28bd6c870751c89ccd7b5ce6ee582
Abhilash107 <abhilash00003108884@gmail.com>
2025-01-27 19:55:41 +0530

    add myArr.cpp to demonstrate contiguous memory allocation with arrays in C++

---
+++ 08_arrays/myArr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Contiguous memory allocation
+    // similar datatype
+    int nums[5] = {1,2,3,4,5};
+    for(int element: nums){
+        cout << element << " ";
+    }
+    // here if we try to access an invalid index of arr, an garbage value is returned
+    cout << endl;
+    for(int i = 0;i < 5;i++){
+        cout << nums[i] << " ";
+    }
+
+
+
+}
