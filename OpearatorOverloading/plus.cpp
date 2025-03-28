709785ff7c4562f36dba8292c2d77e80a319b7aa
Abhilash107 <abhilash00003108884@gmail.com>
2025-03-28 17:32:23 +0530

    operator overloading in Cpp

---
+++ OpearatorOverloading/plus.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class demo{
+    int a;
+    public:
+        void setData(int x){
+            a = x;
+        }
+
+        void getData(){
+            cout << "Data: "<< a << endl;
+        }
+    
+        demo operator+(demo obj){
+            demo res;
+            res.a = a + obj.a;
+            return res;
+        }
+};
+
+int main(){
+    demo a, b, c;
+    a.setData(10);
+    b.setData(20);
+    c = a + b;
+    c.getData();//30
+
+
+
+}
