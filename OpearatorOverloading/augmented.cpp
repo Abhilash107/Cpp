36af0ca4edd5705822f11a9c91ccbaee9ffdd247
Abhilash107 <abhilash00003108884@gmail.com>
2025-04-06 12:23:16 +0530

    add augmented.cpp to demonstrate operator overloading with the += operator

---
+++ OpearatorOverloading/augmented.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class demo{
+    int a;
+    public:
+        void setData(int x){
+            a = x;
+        }
+
+        void getData(){
+            cout << "Data: " << a << endl;
+        }
+
+        void operator+=(demo obj){
+            a = a + obj.a;
+        }
+};
+
+int main(){
+    demo a, b;
+    a.setData(10);
+    b.setData(12);
+    a += b;
+    a.getData();//22
+}
