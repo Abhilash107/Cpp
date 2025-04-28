ba2b51519696e813009b6da7896d6a1c503310e8
Abhilash107 <abhilash00003108884@gmail.com>
2025-04-28 15:57:35 +0530

    refactor Box class and main function for volume calculation; remove Test class

--- test.cpp
+++ test.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class Test {
-    int x = 10;
-    Test(){}
-
-    Test(int x = 5) {
-        cout << x; // Correct: access the class member x
-    }
-
-};
-
-int main() {
-    Test obj(5);
-    
+class Box{
+    public :
+    double length;
+    double breadth;
+    double height;
+    };
+    int main(){
+    Box Box1;
+    double volume;
+    Box1.height = 5;
+    Box1.length = 6;
+    Box1.breadth = 7.1;
+    volume = Box1.height * Box1.length * Box1.
+    breadth;
+    cout << "Volume of Box1 : " << volume <<endl;
     return 0;
-}
+    }
