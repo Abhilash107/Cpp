8a59ee0c8f5619ea357aab47ca70ad4494093411
Abhilash Mishra <abhilash00003108884@gmail.com>
2026-03-08 21:07:32 +0530

    Add diamond problem example and virtual inheritance demonstration

--- practice/oops/Inheritance/multipleInheritanaceEx1.cpp
+++ practice/oops/Inheritance/multipleInheritanaceEx1.cpp
@@ -34,10 +34,49 @@ public:
     }
 };
 
+//! Diamond problem
+class A {
+public:
+    int x;
+};
+
+class B : public A { };
+
+class C : public A { };
+
+class D : public B, public C { };
+
+
+//fixing it
+class A {
+public:
+    int x;
+};
+
+class B : virtual public A { };
+
+class C : virtual public A { };
+
+class D : public B, public C { };
+//But memory layout changes.
+//Instead of two A objects, there is only one shared A.
+//You are telling the compiler:
+//If multiple classes inherit from A, share the same A instance instead of creating duplicates.
+//The compiler internally creates one common base object.
+
 int main() {
 
     C obj;
 
     obj.showA();
     obj.showB();
+
+    Result r;
+
+    r.name = "Rahul";
+    r.roll = 101;
+
+    r.display();
+
+
 }
